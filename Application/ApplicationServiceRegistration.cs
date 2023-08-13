57e29096ba8da5efd7e19495084eaf9770ad6a0d
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-08-13 21:40:16 +0300

    CQRS and MediatR implementation

---
+++ Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // mediatr git bütün assemblyi tara ve command, handlerlarını bulup eşleştirmeni vs. arka plandaki işlerini yap
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
