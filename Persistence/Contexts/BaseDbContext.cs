bb8aba119bbae144ce7923759ac2b4878dc33898
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-09-08 20:40:21 +0300

    Persistence Layer Implementation

---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated(); // veritabanının oluştuğuna emin ol
+    }
+
+    // Brand'i vs. olduğu gibi kullanmak istemiyoruz o yüzden kendi konfigürasyonumuzu yapacağız
+    // Mesela brand hangi alan neye karşılık gelecek vs. gibi ayarları yapılır
+    // İsimlendirme ve ORM'i iyiye kullanmak için böyle yapıyoruz
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        // IEntityTypeConfiguration implement eden sınıfları bulup runtime sırasında konfigrasyona ekler
+    }
+
+}
---
+++ Persistence/EntityConfigurations/BrandConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.EntityConfigurations;
+
+public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+    public void Configure(EntityTypeBuilder<Brand> builder)
+    {
+        builder.ToTable("Brands").HasKey(b => b.Id); // veritabanında karşılık gelecek tablonun adı ve primary keyi
+
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
+        builder.Property(b => b.Name).HasColumnName("Name").IsRequired();
+        builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
+
+        // defaul olarak silinenleri getirmemiz gerekiyor. Soft delete olanları yani. Burada global query filtresi kullanacağız.
+        // Default olarak silinmeyenleri getireceğiz.
+        builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
+    }
+}
